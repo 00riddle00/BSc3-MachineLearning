bef3f1bbcbcc43f6b1cb9fd215f0ef8cd77a28d9
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2021-06-07 15:55:28 +0300

    read input from file, wrap som in a function

--- lab5/som.py
+++ lab5/som.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
-# Self organizing map (SOM)
-# implementation
+
+# ========================================
+# Self organizing map (SOM) implementation
+# (unsupervised learning)
+# ========================================
 
 import math
 import numpy as np
 
+global neighbours
 
-# =========================
+
+# ================
 # Helper functions
-# =========================
+# ================
 
 # convert 1-indexed to 0-indexed
 # ind = index
@@ -19,16 +24,62 @@ def ind(index):
 # calculate Euclidean distance
 # between two numpy arrays
 def euclidean_distance(a, b):
-    print(a)
-    print(type(a))
-    print(b)
-    print(type(b))
-    exit()
     return np.linalg.norm(a - b)
 
 
+# =================================================================
+# Reading from a file
+# =================================================================
+def get_data():
+    data = np.genfromtxt('dataset/iris.data', delimiter=',')
+
+    data_setosa = data[:50]
+    data_versicolor = data[50:100]
+    data_virginica = data[100:150]
+
+    data_train_setosa = data_setosa[:45]
+    data_test_setosa = data_setosa[45:50]
+
+    data_train_versicolor = data_versicolor[:45]
+    data_test_versicolor = data_versicolor[45:50]
+
+    data_train_virginica = data_virginica[:45]
+    data_test_virginica = data_virginica[45:50]
+
+    # -------------------- Training data --------------------------
+    given_train_inputs = []
+
+    for element in zip(*[data_train_setosa, data_train_versicolor,
+                         data_train_virginica]):
+        given_train_inputs.extend(element)
+
+    given_train_inputs = np.array(given_train_inputs, np.float16)
+
+    given_train_outputs = np.ones(135, np.uint8)
+
+    # every second element is of class 1
+    given_train_outputs[1::3] = 2
+    given_train_outputs[2::3] = 3
+    given_train_outputs = given_train_outputs.reshape(1, -1).T
+
+    # -------------------- Testing data ---------------------------
+    given_test_inputs = np.array(np.concatenate((
+        data_test_setosa, data_test_versicolor, data_test_virginica, data)),
+        np.float16)
+
+    given_test_outputs = np.ones(15, np.uint8)
+    given_test_outputs[5:10] = 2
+    given_test_outputs[10:] = 3
+    given_test_outputs = given_test_outputs.reshape(1, -1).T
+
+    return [given_train_inputs,
+            given_train_outputs,
+            given_test_inputs,
+            given_test_outputs]
+
+
 # =========================
-# Main functions
+# Main program functions
 # =========================
 
 # learning rate function
@@ -45,8 +96,18 @@ def alpha_fn(t, T, kind=1):
         return math.pow(0.005, (t / T))
 
 
+def eta_c(c, ij):
+    d = euclidean_distance(c, ij)
+    if d == 0:
+        return 1
+
+    return math.floor(d)
+
+
 # check if (i,j) is in a vicinity to c
 def N_c(c, i, j, t, e, n=3):
+    global neighbours
+
     vicinity = 0
 
     vicinity_neighbours = set(neighbours[c])
@@ -71,14 +132,6 @@ def N_c(c, i, j, t, e, n=3):
     return False
 
 
-def eta_c(c, ij):
-    d = euclidean_distance(c, ij)
-    if d == 0:
-        return 1
-
-    return math.floor(d)
-
-
 # neighbour function
 # @param c - neuron leader
 # @params i,j - indices (1-indexed)
@@ -100,64 +153,55 @@ def h(c, i, j, t, e, kind='bubble'):
         return alpha_fn(t, e) * np.exp(nominator / denominator)
 
 
+def som_train(X, M, e, kx, ky):
+    distances = np.zeros((kx, ky))
+    for t in range(1, e + 1):
+        print(f'Epoch {t}')
+        for l in range(1, m + 1):  # for every input vector 'l'
+            for i in range(1, kx + 1):
+                for j in range(1, ky + 1):
+                    # calculate euclidean distance
+                    distances[ind(i)][ind(j)] = \
+                        euclidean_distance(M[ind(i)][ind(j)], X[ind(l)])
+            # c = neuron leader
+            c = np.unravel_index(distances.argmin(), distances.shape)
+            for i in range(1, kx + 1):
+                for j in range(1, ky + 1):
+                    # Update neurons using SOM learning rule
+                    Mij = M[ind(i)][ind(j)]
+                    M[ind(i)][ind(j)] = Mij + h(c, ind(i), ind(j), t, e) * (X[ind(l)] - Mij)
+    return M
+
+
 # =========================
 # Main code
 # =========================
+if __name__ == '__main__':
+    data = get_data()
 
-# input vectors
-X = np.array([[5.1, 3.5, 1.4, 0.2],
-              [4.9, 3.0, 1.4, 0.2],
-              [4.7, 3.2, 1.3, 0.2],
-              [4.6, 3.1, 1.5, 0.0],
-              [7.0, 3.2, 4.7, 1.4],
-              [6.4, 3.2, 4.5, 1.5],
-              [6.9, 3.1, 4.9, 1.5],
-              [5.5, 2.3, 4.0, 1.0],
-              [6.3, 3.3, 6.0, 2.5],
-              [5.8, 2.7, 5.1, 1.9],
-              [7.1, 3.0, 5.9, 2.1],
-              [6.3, 2.9, 5.6, 1.8]], np.float16)
+    # train inputs (vectors)
+    X = data[0]
+    X = np.delete(X, 4, axis=1)
 
-# classes of input vectors
-labels = np.array([0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2], np.uint8)
+    test_inputs = data[2]
+    test_labels = data[3]
 
-e = 100  # epoch count
-kx = 4  # grid rows
-ky = 4  # grid columns
-m = X.shape[0]  # no of input vectors
-n = X.shape[1]  # attribute count of the input vector
-M = np.random.rand(kx, ky, n)  # grid neurons
+    m = X.shape[0]  # no of input vectors
+    n = X.shape[1]  # how many attributes the input vector has
 
-# collect euclidean distances
-distances = np.zeros((kx, ky))
+    kx = 8  # grid rows
+    ky = 8  # grid columns
+    np.random.seed(0)
+    M = np.random.rand(kx, ky, n)  # grid neurons
 
-ROWS = kx
-COLS = ky
+    epochs = 20
 
-# create neighbours grid
-neighbours = {
-    (p // COLS, p % COLS): [(p // COLS + x_inc - 1, p % COLS + y_inc - 1)
-                            for x_inc in range(3) if 1 <= p // COLS + x_inc <= ROWS
-                            for y_inc in range(3) if 1 <= p % COLS + y_inc <= COLS and not y_inc == x_inc == 1]
-    for p in range(ROWS * COLS)}
+    # create neighbours grid
+    neighbours = {
+        (p // ky, p % ky): [(p // ky + x_inc - 1, p % ky + y_inc - 1)
+                            for x_inc in range(3) if 1 <= p // ky + x_inc <= kx
+                            for y_inc in range(3) if 1 <= p % ky + y_inc <= ky and not y_inc == x_inc == 1]
+        for p in range(kx * ky)}
 
-# TODO def som_training(X_data, X_labels, M, e, kx, ky): -> return M
-for t in range(1, e + 1):
-    for l in range(1, m + 1):  # for every input vector 'l'
-        for i in range(1, kx + 1):
-            for j in range(1, ky + 1):
-                # calculate euclidean distance
-                distances[ind(i)][ind(j)] = \
-                    euclidean_distance(M[ind(i)][ind(j)], X[ind(l)])
-        # c = neuron leader
-        c = np.unravel_index(distances.argmin(), distances.shape)
-        # c = (c[0] + 1, c[1] + 1)  # restore 1-indexed notation
-        # c = (c[0] + 1, c[1] + 1)  # restore 1-indexed notation
-        for i in range(1, kx + 1):
-            for j in range(1, ky + 1):
-                print('i==', i, 'j==', j)
-                # Update neurons using SOM learning rule
-                Mij = M[ind(i)][ind(j)]
-                M[ind(i)][ind(j)] = Mij + h(c, ind(i), ind(j), t, e) * (X[ind(l)] - Mij)
-
-print(M)
+    trained_M = som_train(X, M, epochs, kx, ky)
+    print(trained_M)
